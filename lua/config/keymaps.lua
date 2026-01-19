cd19619bf81b3d591a0018f65e9a7ccb2db665ff
jie yang <topelinux@gmail.com>
2026-01-19 08:56:34 +0800

    add zh keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,6 +11,8 @@ map("n", "<leader>3", function()
   Snacks.picker.keymaps()
 end, { desc = "Keymaps by snask" })
 map("n", "<leader>fa", "<cmd>Neotree toggle left reveal_force_cwd=true<CR>", { desc = "reveal in tree" })
+map("n", "<leader>zh", ":edit ++enc=gb18030<CR>")
+
 --map("n", "<space>y", '"*y', { desc = "copy to system" })
 --map("n", "<leader>ss", '<cmd>setlocal spell!<cr>', { desc = "set spell" })
 --vim.cmd([[
