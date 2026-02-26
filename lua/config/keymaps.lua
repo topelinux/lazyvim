ea5f99a1dc799e61d1974cf38d33ffd40131f9fa
jie yang <topelinux@gmail.com>
2026-02-26 21:25:42 +0800

    Update map for yank

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,7 +13,9 @@ end, { desc = "Keymaps by snask" })
 map("n", "<leader>fa", "<cmd>Neotree toggle left reveal_force_cwd=true<CR>", { desc = "reveal in tree" })
 map("n", "<leader>zh", ":edit ++enc=gb18030<CR>")
 
---map("n", "<space>y", '"*y', { desc = "copy to system" })
+map({ "n", "v" }, "<leader>y", '"*y', { desc = "copy to system" })
+--map("v", "<leader>y", '"*y', { desc = "copy to system" })
+--map("n", "<leader>y", '"+y', { desc = "copy to system" })
 --map("n", "<leader>ss", '<cmd>setlocal spell!<cr>', { desc = "set spell" })
 --vim.cmd([[
 --nnoremap <silent> <leader>k <cmd> Man<CR>
